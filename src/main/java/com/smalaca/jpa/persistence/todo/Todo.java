2a1ea8b0dbb59dde58f5855a1d1afc036bdaa020
smalaca <ab13.krakow@gmail.com>
2022-07-02 14:56:58 +0200

    use @PostLoad to calculate value of @Transient field in Todo entity

--- src/main/java/com/smalaca/jpa/persistence/todo/Todo.java
+++ src/main/java/com/smalaca/jpa/persistence/todo/Todo.java
@@ -3,10 +3,11 @@ package com.smalaca.jpa.persistence.todo;
 
 import lombok.ToString;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.PostLoad;
+import javax.persistence.Transient;
 import java.util.UUID;
 
 @Entity
@@ -16,13 +17,21 @@ public class Todo {
     @GeneratedValue
     private UUID id;
 
-    @Column(name = "name")
     private String subject;
+
+    @Transient
+    private String firstLetterOfSubject;
 
     private Todo() {}
 
     public Todo(String subject) {
         this.subject = subject;
+        calculateFirstLetterOfSubject();
+    }
+
+    @PostLoad
+    private void calculateFirstLetterOfSubject() {
+        firstLetterOfSubject = subject.substring(0, 1);
     }
 
     void changeSubjectTo(String subject) {
