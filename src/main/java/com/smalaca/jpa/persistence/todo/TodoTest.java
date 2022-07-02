124b9ac244ad4fe181fbcae70690586d952bd68b
smalaca <ab13.krakow@gmail.com>
2022-07-02 11:43:32 +0200

    add comments to visualize what TodoRepository replaces

--- src/main/java/com/smalaca/jpa/persistence/todo/TodoTest.java
+++ src/main/java/com/smalaca/jpa/persistence/todo/TodoTest.java
@@ -8,13 +8,22 @@ public class TodoTest {
     private static final RepositoriesFactory REPOSITORIES_FACTORY = RepositoriesFactory.create();
 
     public static void main(String[] args) {
+//        EntityManager entityManager;
         TodoRepository todoRepositoryOne = REPOSITORIES_FACTORY.toDoRepository();
         UUID todoIdOne = UUID.randomUUID();
         todoRepositoryOne.save(new Todo(todoIdOne, "conduct JPA training"));
         todoRepositoryOne.save(new Todo(UUID.randomUUID(), "conduct ORM training"));
+
+//        EntityTransaction transaction = entityManager.getTransaction();
+//        transaction.begin();
+//        entityManager.persist(new Todo(UUID.randomUUID(), "conduct Hibernate training"));
+//        transaction.commit();
         todoRepositoryOne.save(new Todo(UUID.randomUUID(), "conduct Hibernate training"));
 
         TodoRepository todoRepositoryTwo = REPOSITORIES_FACTORY.toDoRepository();
-        System.out.println(todoRepositoryTwo.findById(todoIdOne));
+
+//        entityManager.find(Todo.class, todoIdOne)
+        Todo byId = todoRepositoryTwo.findById(todoIdOne);
+        System.out.println(byId);
     }
 }
