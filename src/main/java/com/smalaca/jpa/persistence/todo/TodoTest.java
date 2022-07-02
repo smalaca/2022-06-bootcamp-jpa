14c1593c07aee2cfd13f58d81291f2a907aaf895
smalaca <ab13.krakow@gmail.com>
2022-07-02 10:25:10 +0200

    Use flush to force insert of Todo entity

--- src/main/java/com/smalaca/jpa/persistence/todo/TodoTest.java
+++ src/main/java/com/smalaca/jpa/persistence/todo/TodoTest.java
@@ -2,6 +2,7 @@ package com.smalaca.jpa.persistence.todo;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.util.UUID;
 
@@ -10,13 +11,20 @@ public class TodoTest {
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("ToDo");
         EntityManager entityManager = factory.createEntityManager();
 
-        entityManager.getTransaction().begin();
-        Todo todo = new Todo(UUID.randomUUID(), "conduct JPA training");
-        entityManager.persist(todo);
+        EntityTransaction transaction = entityManager.getTransaction();
 
-        entityManager.getTransaction().commit();
+        transaction.begin();
+        entityManager.persist(new Todo(UUID.randomUUID(), "conduct JPA training"));
+        System.out.println("BEFORE FLUSH");
+        entityManager.flush();
+        System.out.println("AFTER FLUSH");
+        entityManager.persist(new Todo(UUID.randomUUID(), "conduct ORM training"));
+        entityManager.persist(new Todo(UUID.randomUUID(), "conduct Hibernate training"));
+
+        System.out.println("BEFORE COMMIT");
+        transaction.commit();
 
         entityManager.close();
-        System.out.println(todo);
+        System.out.println(new Todo(UUID.randomUUID(), "conduct JPA training"));
     }
 }
