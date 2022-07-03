a71e5620d59eb6048c6757f580900da48a8819a2
smalaca <ab13.krakow@gmail.com>
2022-07-03 16:38:57 +0200

    add AddressRepository.findAllByCity method

--- src/main/java/com/smalaca/jpa/springdata/AddressRepository.java
+++ src/main/java/com/smalaca/jpa/springdata/AddressRepository.java
@@ -2,5 +2,8 @@ package com.smalaca.jpa.springdata;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface AddressRepository extends CrudRepository<Address, Long> {
+    List<Address> findAllByCity(String city);
 }
--- src/main/java/com/smalaca/jpa/springdata/JpaListener.java
+++ src/main/java/com/smalaca/jpa/springdata/JpaListener.java
@@ -23,6 +23,14 @@ public class JpaListener {
         addressRepository.save(new Address("Krakowska 67", "43212", "Warszawa", "Polska"));
         addressRepository.save(new Address("Lubicz 7/7", "12345", "Kraków", "Polska"));
 
+        System.out.println("---- FIND ALL ----");
         addressRepository.findAll().forEach(System.out::println);
+        System.out.println("---- FIND FOR KRAKÓW ----");
+        addressRepository.findAllByCity("Kraków").forEach(System.out::println);
+        System.out.println("---- FIND FOR WARSZAWA ----");
+        addressRepository.findAllByCity("Warszawa").forEach(System.out::println);
+        System.out.println("---- FIND FOR GDYNIA ----");
+        addressRepository.findAllByCity("Gdynia").forEach(System.out::println);
+        System.out.println("---- KONIEC ----");
     }
 }
