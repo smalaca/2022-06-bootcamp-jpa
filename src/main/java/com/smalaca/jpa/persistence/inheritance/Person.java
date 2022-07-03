b6f10d701c8f2163745a19003dd9fff9c4eeaba8
smalaca <ab13.krakow@gmail.com>
2022-07-03 15:57:16 +0200

    inheritance - use strategy to keep tables for each entity

--- src/main/java/com/smalaca/jpa/persistence/inheritance/Person.java
+++ src/main/java/com/smalaca/jpa/persistence/inheritance/Person.java
@@ -1,9 +1,14 @@
 package com.smalaca.jpa.persistence.inheritance;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import java.util.UUID;
 
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Person {
     @Id
     @GeneratedValue
